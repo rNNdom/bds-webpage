f0474f34ce5dc259d05d39bf999a85488920c624
rNNdom <l.galeano01@ufromail.cl>
2024-07-11 22:21:47 -0400

    feature: implemented lambda function consumption from aws, and SES service

--- components/ContactUs.tsx
+++ components/ContactUs.tsx
@@ -5,11 +5,41 @@ import { Section, Container } from './craft'
 import { Button } from './ui/button'
 
 function ContactUs() {
-  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+  // const handleSubmit = async (e) => {
+  //   e.preventDefault()
+  //   console.log(to, subject, body)
+  //   const response = await fetch('https://ea9aj1j2ub.execute-api.us-east-1.amazonaws.com/TestSES/send-email', {
+  //     method: 'POST',
+  //     headers: {
+  //       'Content-Type': 'application/json'
+  //     },
+  //     body: JSON.stringify({ to, subject, body })
+  //   })
+
+  //   if (response.ok) {
+  //     alert('Email sent successfully!')
+  //   } else {
+  //     alert('Error sending email.')
+  //   }
+  // }
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault()
     const formData = new FormData(e.currentTarget)
     const data = Object.fromEntries(formData.entries())
-    console.log(data, 'yeayayaya')
+    const response = await fetch('https://ea9aj1j2ub.execute-api.us-east-1.amazonaws.com/TestSES/send-email', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({ ...data })
+    })
+    if (response.ok) {
+      alert('Email sent successfully!')
+    } else {
+      alert('Error sending email.')
+    }
+
+    console.log(data, { ...data })
   }
   return (
     <Section className='text-oxfordBlue  md:pt-0 pb-0 md:pb-0 not-prose '>
@@ -19,9 +49,9 @@ function ContactUs() {
         </h3>
 
         <form className='flex flex-col' onSubmit={handleSubmit}>
-          <input name='user_name' type='text' placeholder='Tu nombre' required className='mb-2 p-2 border rounded' />
-          <input name='user_email' type='email' placeholder='Correo electrónico' required className='mb-2 p-2 border rounded' />
-          <textarea name='user_issue' placeholder='Tu mensaje (Max. 500 caracteres)' maxLength={500} required className='mb-2 p-2 border rounded h-48 resize-none' />
+          <input name='name' type='text' placeholder='Tu nombre' required className='mb-2 p-2 border rounded' />
+          <input name='email' type='email' placeholder='Correo electrónico' required className='mb-2 p-2 border rounded' />
+          <textarea name='message' placeholder='Tu mensaje (Max. 500 caracteres)' maxLength={500} required className='mb-2 p-2 border rounded h-48 resize-none' />
           <span className='flex justify-center'>
             <Button type='submit' className='hidden sm:flex bg-buttonTealColor font-semibold text-md w-1/2 '>
               Enviar
