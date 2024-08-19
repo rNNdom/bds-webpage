67afec301d34f6534aae09a7d9a2b1bfa27e674e
rNNMdom <l.galeano01@ufromail.cl>
2024-08-19 18:06:37 -0400

    feat(solutions): finished solutions view

--- components/SectionTitle.tsx
+++ components/SectionTitle.tsx
@@ -2,13 +2,13 @@ import { cn } from '@/lib/utils'
 import React from 'react'
 import Balancer from 'react-wrap-balancer'
 
-export default function SectionTitle({ children, width = 'w-1/2', className }: { children: string; width?: string; className?: string }) {
+export default function SectionTitle({ children, width = 'w-1/2', className, borderColor }: { children: string; width?: string; className?: string; borderColor?: string }) {
   return (
     <div>
       <h3 className={cn('text-4xl font-bold text-center w-full', className)}>
         <Balancer>{children}</Balancer>
       </h3>
-      <span className={`block border-bgGreenColor border-b-4 ${width} pt-2`} />
+      <span className={`${borderColor} ${width} block border-bgGreenColor border-b-4  pt-2 `} />
     </div>
   )
 }
--- components/solutions-view/SolutionsCofluenceOpenAi.tsx
+++ components/solutions-view/SolutionsCofluenceOpenAi.tsx
@@ -3,22 +3,25 @@ import { Container, Section } from '@/components/craft'
 
 import Balancer from 'react-wrap-balancer'
 import Image from 'next/image'
-import BDS4 from '@/public/BDS4.png'
+import Logo from '@/public/service-logos/MLLogo.svg'
+import SectionTitle from '../SectionTitle'
 
 export default function SolutionsCofOpenAI() {
   return (
-    <Section className='text-oxfordBlue  md:pt-0 pb-0 md:pb-0 not-prose'>
-      <Container className='text-center sm:p-2 not-prose'>
-        <div className='flex flex-row items-center justify-around'>
-          <div className='border rounded-full overflow-hidden '>
-            <Image src={BDS4} alt='placeholder' className='fill object-cover bg-white h-56' width={450} height={350} />
+    <Section className='relative z-10 text-oxfordBlue  md:pt-0 pb-0 md:pb-0 not-prose'>
+      <Container className='text-center not-prose'>
+        <div className='flex flex-col-reverse md:flex-row-reverse items-center justify-around'>
+          <div>
+            <Image src={Logo} alt='placeholder' className='object-contain' width={170} height={170} />
           </div>
-          <div className='pl-10 w-3/6'>
-            <h4 className='text-3xl font-bold text-center'>
-              <Balancer>Cofluence + OpenAI</Balancer>
-            </h4>
+          <div className='w-full md:w-4/6 '>
+            <span className='flex justify-start  w-full'>
+              <SectionTitle width='w-1/2' className='text-3xl'>
+                Cofluence + OpenAI
+              </SectionTitle>
+            </span>
             <p className='mb-4 pt-5'>
-              <Balancer className='text-end text-xl'>
+              <Balancer className='text-justify md:text-start text-xl font-medium'>
                 Proporciona la capacidad de <strong>leer y transformar documentos desde la plataforma Cofluence</strong>, y mediante <strong>OpenAI</strong>, permite{' '}
                 <strong>interactuar con los usuarios para resolver sus consultas</strong> de carácter general y específico. Esta integración optimiza el flujo de trabajo al{' '}
                 <strong>automatizar procesos complejos</strong> y <strong>mejorar la precisión en la gestión de información</strong>, brindando a los usuarios una{' '}
